2f4eba64b1fe15ed079d9062f17c8ef1e4418768
hnthap <94937751+hnthap@users.noreply.github.com>
2026-04-17 20:00:10 +0700

    Strip the protocol from the URL fed into docker commands

--- vars/deployNexus.groovy
+++ vars/deployNexus.groovy
@@ -1,12 +1,16 @@
 def call(String registryUrl) {
+  // Strip http:// or https:// from the beginning of the URL
+  String cleanRegistry = registryUrl.replaceFirst("^https?://", "")
+
   sh './mvnw package -DskipTests'
-  sh "docker build -t ${registryUrl}/petclinic:latest ."
+  sh "docker build -t ${cleanRegistry}/petclinic:latest ."
+
   withCredentials([usernamePassword(
     credentialsId: 'nexus-credentials',
     usernameVariable: 'USER',
     passwordVariable: 'PASS'
   )]) {
-    sh "echo \$PASS | docker login ${registryUrl} -u \$USER --password-stdin"
-    sh "docker push ${registryUrl}/petclinic:latest"
+    sh "echo \$PASS | docker login ${cleanRegistry} -u \$USER --password-stdin"
+    sh "docker push ${cleanRegistry}/petclinic:latest"
   }
 }
