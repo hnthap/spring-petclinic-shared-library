0a9b80873623f5b655f4bba4a27c745b2e3e56c0
hnthap <94937751+hnthap@users.noreply.github.com>
2026-04-21 10:39:01 +0700

    Deploy container to remote production node via SSH.
    Updates the `deployApp.groovy` step to execute deployment commands on a remote server (`env.APP_TARGET_IP`) instead of the local Jenkins build runner.
    
    * Added `sshUserPrivateKey` binding to load the `azure-ssh-key` credential.
    * Wrapped the `docker login`, `rm`, `pull`, and `run` commands inside an SSH Here-Doc block to execute them directly on the target machine.

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -3,16 +3,25 @@ def call(String branchName, String registryUrl) {
   def envName = branchName == 'main' ? 'prod' : 'uat'
   String cleanRegistry = registryUrl.replaceFirst("^https?://", "")
   
-  withCredentials([usernamePassword(
-    credentialsId: 'nexus-credentials',
-    usernameVariable: 'USER',
-    passwordVariable: 'PASS'
-  )]) {
+  withCredentials([
+    usernamePassword(
+      credentialsId: 'nexus-credentials',
+      usernameVariable: 'USER',
+      passwordVariable: 'PASS'
+    ),
+    sshUserPrivateKey(
+      credentialsId: 'azure-ssh-key',
+      keyFileVariable: 'SSH_KEY',
+      usernameVariable: 'SSH_USER',
+    )
+  ]) {
     sh """
-    echo \$PASS | docker login ${cleanRegistry} -u \$USER --password-stdin
-    docker rm -f petclinic-${envName} || true
-    docker pull ${cleanRegistry}/petclinic:latest
-    docker run -d -p ${port}:8080 --name petclinic-${envName} --restart unless-stopped ${cleanRegistry}/petclinic:latest
+    ssh -i \$SSH_KEY -o StrictHostKeyChecking=no \$SSH_USER@${env.APP_TARGET_IP} << EOF
+      echo \$PASS | docker login ${cleanRegistry} -u \$USER --password-stdin
+      docker rm -f petclinic-${envName} || true
+      docker pull ${cleanRegistry}/petclinic:latest
+      docker run -d -p ${port}:8080 --name petclinic-${envName} --restart unless-stopped ${cleanRegistry}/petclinic:latest
+EOF
     """
   }
 }
